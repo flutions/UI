6ddfefdd1b2cbed1042144cfda0fab1ae2e615d1
Flutions <admin@flutions.in>
2022-08-18 19:22:50 +0530

    Header and Banner
    Header and banner section is completed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { BannerComponent } from './body/banner/banner.component';
+import { ProcessComponent } from './body/process/process.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    BannerComponent,
+    ProcessComponent
   ],
   imports: [
     BrowserModule,
